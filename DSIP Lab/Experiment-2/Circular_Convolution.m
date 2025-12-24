7cb374ec8d5008750123d503346ed55d612d2176
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 18:23:44 -0500

    DSIP

--- DSIP Lab/Experiment-2/Circular_Convolution.m
+++ DSIP Lab/Experiment-2/Circular_Convolution.m
@@ -1,13 +1,36 @@
 ﻿% EXPERIMENT 2: CIRCULAR CONVOLUTION
 % Author: Amey Thakur
 % GitHub: https://github.com/Amey-Thakur
-% Course: CSL701 | Roll No: 50 | Batch: B3
-% Date: 30/07/2021
+% Repository: https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% Course: Digital Signal and Image Processing Lab (CSL701)
+% Roll No: 50 | Batch: B3 | Date: 30/07/2021
+%
+% Aim: Perform Circular Convolution using DFT/IDFT method
+% Formula: y[n] = IDFT(X[k] * H[k])
 
+% Input signal x[n] with 4 samples
 Xn = [0 1 2 3];
+
+% Impulse response h[n] with 4 samples (must be same length for circular)
 Yn = [1 2 0 -1];
+
+% Step 1: Compute DFT of x[n] using FFT
 x = fft(Xn);
+
+% Step 2: Compute DFT of h[n] using FFT
 y = fft(Yn);
-C = x.*y;
+
+% Step 3: Multiply DFTs element-wise in frequency domain
+C = x .* y;
+
+% Step 4: Compute IDFT to get circular convolution result
 T = ifft(C);
+
+% Plot the result as stem plot
 stem(T, 'filled');
+
+% Add labels for better visualization
+xlabel('n');
+ylabel('y[n]');
+title('Circular Convolution Result');
+grid on;
--- DSIP Lab/Experiment-2/Linear_Convolution.m
+++ DSIP Lab/Experiment-2/Linear_Convolution.m
@@ -1,11 +1,28 @@
 ﻿% EXPERIMENT 2: LINEAR CONVOLUTION
 % Author: Amey Thakur
 % GitHub: https://github.com/Amey-Thakur
-% Course: CSL701 | Roll No: 50 | Batch: B3
-% Date: 30/07/2021
+% Repository: https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% Course: Digital Signal and Image Processing Lab (CSL701)
+% Roll No: 50 | Batch: B3 | Date: 30/07/2021
+%
+% Aim: Perform Discrete Linear Convolution of two signals
+% Formula: y[n] = x[n] * h[n], Output length = M + N - 1
 
+% Input signal x[n] with 4 samples
 Xn = [1 3 2 -1];
+
+% Impulse response h[n] with 3 samples  
 Yn = [1 -1 3];
+
+% Compute linear convolution using conv() function
+% Output will have length = 4 + 3 - 1 = 6 samples
 C = conv(Xn, Yn);
-n = 1:8;
+
+% Plot the convolution result as stem plot
 stem(C, 'filled');
+
+% Add labels for better visualization
+xlabel('n');
+ylabel('y[n]');
+title('Linear Convolution Result');
+grid on;
