739fc8ed47ad4ba97db45132bc484fe32649fabb
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 20:41:30 -0500

    DSIP

--- DSIP Lab/Experiment-2/Circular_Convolution.m
+++ DSIP Lab/Experiment-2/Circular_Convolution.m
@@ -1,54 +1,62 @@
-﻿% == == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == ==
-    = % DIGITAL SIGNAL AND IMAGE PROCESSING LAB % % Experiment No. : 2 %
-              Experiment Title
-    : Linear and Circular Convolution using FFT %
-      % Aim : % Write a program to perform Discrete Convolution such as %
-              Linear Convolution and Circular Convolution.% %
-              Student Details : % Name : Amey Thakur % Roll No. : 50 %
-                                         Class : COMPS -
-          BE - B -
-          50 % Batch : B3 % % Date of Experiment : 30 / 07 / 2021 %
-                       Date of Submission : 30 / 07 / 2021 % %
-                       Author : % Amey Thakur % GitHub Profile : %
-                                                                 https
-    : // github.com/Amey-Thakur
-      %
-      % Repository : %
-                     https
-    : // github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
-      %
-      == == == == == == == == == == == == == == == == == == == == == == == == ==
-      == == == == == == == == ==
-    =
+% =====================================================================
+% DIGITAL SIGNAL AND IMAGE PROCESSING LAB
+%
+% Experiment No. : 2
+% Experiment Title : Linear and Circular Convolution using FFT
+%
+% Aim:
+% Write a program to perform Discrete Convolution such as
+% Linear Convolution and Circular Convolution.
+%
+% Student Details:
+% Name     : Amey Thakur
+% Roll No. : 50
+% Class    : COMPS-BE-B-50
+% Batch    : B3
+%
+% Date of Experiment : 30/07/2021
+% Date of Submission : 30/07/2021
+%
+% Author:
+% Amey Thakur
+% GitHub Profile:
+% https://github.com/Amey-Thakur
+%
+% Repository:
+% https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% =====================================================================
 
-        % Clear workspace,
-    close all figures, and clear command window clear all;
+% Clear workspace, close all figures, and clear command window
+clear all;
 close all;
 clc;
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Input Discrete -
-    Time Signals % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -Xn = [0 1 2 3];
-% First input sequence Yn = [1 2 0 - 1];
-% Second input sequence
+% -----------------------------
+% Input Discrete-Time Signals
+% -----------------------------
+Xn = [0 1 2 3];        % First input sequence
+Yn = [1 2 0 -1];       % Second input sequence
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% FFT of Input Signals %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -x = fft(Xn);
-% FFT of first signal y = fft(Yn);
-% FFT of second signal
+% -----------------------------
+% FFT of Input Signals
+% -----------------------------
+x = fft(Xn);           % FFT of first signal
+y = fft(Yn);           % FFT of second signal
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-    Convolution in Frequency Domain %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -C = x.*y;
-% Point - wise multiplication
+% -----------------------------
+% Convolution in Frequency Domain
+% -----------------------------
+C = x .* y;            % Point-wise multiplication
 
-              % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-              Inverse FFT(Circular Convolution) %
-              -- -- -- -- -- -- -- -- -- -- -- -- -- -- -T = ifft(C);
-% Circular convolution result
+% -----------------------------
+% Inverse FFT (Circular Convolution)
+% -----------------------------
+T = ifft(C);           % Circular convolution result
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Plot the Output %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
+% -----------------------------
+% Plot the Output
+% -----------------------------
+figure;
 stem(T, 'filled');
 grid on;
 title('Circular Convolution using FFT');
