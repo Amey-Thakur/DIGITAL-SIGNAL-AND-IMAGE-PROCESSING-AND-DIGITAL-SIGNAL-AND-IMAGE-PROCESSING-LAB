ed3e1d1844e56b92f2ca18436d4a5c3326abdfad
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 21:52:22 -0500

    DSIP

--- DSIP Lab/Experiment-9/Image_Smoothing.m
+++ DSIP Lab/Experiment-9/Image_Smoothing.m
@@ -1,47 +1,55 @@
-% == == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == ==
-    = % DIGITAL SIGNAL AND IMAGE PROCESSING LAB % % Experiment No. : 9 %
-              Experiment Title : Image Smoothing and Image Sharpening % %
-                                 Aim
-    : %
-      Write a program to perform Image Smoothing and Image Sharpening.% %
-      Student Details : % Name : Amey Thakur % Roll No. : 50 % Class : COMPS -
-          BE - B -
-          50 % Batch : B3 % % Date of Experiment : 06 / 10 / 2021 %
-                       Date of Submission : 06 / 10 / 2021 % %
-                       Author : % Amey Thakur % GitHub Profile : %
-                                                                 https
-    : // github.com/Amey-Thakur
-      %
-      % Repository : %
-                     https
-    : // github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
-      %
-      == == == == == == == == == == == == == == == == == == == == == == == == ==
-      == == == == == == == == ==
-    =
+% =====================================================================
+% DIGITAL SIGNAL AND IMAGE PROCESSING LAB
+%
+% Experiment No. : 9
+% Experiment Title : Image Smoothing and Image Sharpening
+%
+% Aim:
+% Write a program to perform Image Smoothing and Image Sharpening.
+%
+% Student Details:
+% Name     : Amey Thakur
+% Roll No. : 50
+% Class    : COMPS-BE-B-50
+% Batch    : B3
+%
+% Date of Experiment : 06/10/2021
+% Date of Submission : 06/10/2021
+%
+% Author:
+% Amey Thakur
+% GitHub Profile:
+% https://github.com/Amey-Thakur
+%
+% Repository:
+% https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% =====================================================================
 
-        % Clear workspace,
-    close all figures, and clear command window clc;
+% Clear workspace, close all figures, and clear command window
+clc;
 clear all;
 close all;
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Read Input Image %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -I = imread('cameraman.tif');
-% Read grayscale image
+% -----------------------------
+% Read Input Image
+% -----------------------------
+I = imread('cameraman.tif');     % Read grayscale image
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-    Image Smoothing using Gaussian Filter %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -Iblur = imgaussfilt(I, 2);
-% Apply Gaussian smoothing(sigma = 2)
+% -----------------------------
+% Image Smoothing using Gaussian Filter
+% -----------------------------
+Iblur = imgaussfilt(I, 2);       % Apply Gaussian smoothing (sigma = 2)
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Display Smoothed Image %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
+% -----------------------------
+% Display Smoothed Image
+% -----------------------------
+figure;
 imshow(Iblur);
 title('Smoothed Image using Gaussian Filter');
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-    Compare Original and Smoothed Images %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
+% -----------------------------
+% Compare Original and Smoothed Images
+% -----------------------------
+figure;
 montage({I, Iblur});
 title('Original Image (Left) vs Gaussian Filtered Image (Right)');
