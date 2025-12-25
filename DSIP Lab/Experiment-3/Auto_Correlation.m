f19797f58a816943d92eb723ab3e4f4c07c9dbde
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 20:52:12 -0500

    DSIP

--- DSIP Lab/Experiment-3/Auto_Correlation.m
+++ DSIP Lab/Experiment-3/Auto_Correlation.m
@@ -1,54 +1,62 @@
-% == == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == ==
-    = % DIGITAL SIGNAL AND IMAGE PROCESSING LAB % % Experiment No. : 3 %
-              Experiment Title
-    : Discrete Correlation of Signals %
-      % Aim : % Write a program to perform the mathematical operation %
-              Discrete Correlation and measure the degree of similarity %
-              between two signals.% % Student Details : % Name : Amey Thakur %
-                                                                 Roll No. : 50 %
-                                                                 Class : COMPS -
-          BE - B -
-          50 % Batch : B3 % % Date of Experiment : 11 / 08 / 2021 %
-                       Date of Submission : 11 / 08 / 2021 % %
-                       Author : % Amey Thakur % GitHub Profile : %
-                                                                 https
-    : // github.com/Amey-Thakur
-      %
-      % Repository : %
-                     https
-    : // github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
-      %
-      == == == == == == == == == == == == == == == == == == == == == == == == ==
-      == == == == == == == == ==
-    =
+% =====================================================================
+% DIGITAL SIGNAL AND IMAGE PROCESSING LAB
+%
+% Experiment No. : 3
+% Experiment Title : Discrete Correlation of Signals
+%
+% Aim:
+% Write a program to perform the mathematical operation
+% Discrete Correlation and measure the degree of similarity
+% between two signals.
+%
+% Student Details:
+% Name     : Amey Thakur
+% Roll No. : 50
+% Class    : COMPS-BE-B-50
+% Batch    : B3
+%
+% Date of Experiment : 11/08/2021
+% Date of Submission : 11/08/2021
+%
+% Author:
+% Amey Thakur
+% GitHub Profile:
+% https://github.com/Amey-Thakur
+%
+% Repository:
+% https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% =====================================================================
 
-        % Clear workspace,
-    close all figures, and clear command window clear all;
+% Clear workspace, close all figures, and clear command window
+clear all;
 close all;
 clc;
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Input Discrete -
-    Time Signal % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -x = [4 8 6 3];
-% Input signal sequence
+% -----------------------------
+% Input Discrete-Time Signal
+% -----------------------------
+x = [4 8 6 3];        % Input signal sequence
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Plot Original Signal %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
-subplot(2, 1, 1);
+% -----------------------------
+% Plot Original Signal
+% -----------------------------
+figure;
+subplot(2,1,1);
 stem(x, 'filled');
 grid on;
 title('Original Discrete-Time Signal');
 xlabel('Sample Index');
 ylabel('Amplitude');
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Auto -
-    Correlation of Signal % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -y =
-    xcorr(x, x);
-% Auto - correlation of signal x
+% -----------------------------
+% Auto-Correlation of Signal
+% -----------------------------
+y = xcorr(x, x);     % Auto-correlation of signal x
 
-             % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-             Plot Correlation Output %
-             -- -- -- -- -- -- -- -- -- -- -- -- -- -- -subplot(2, 1, 2);
+% -----------------------------
+% Plot Correlation Output
+% -----------------------------
+subplot(2,1,2);
 stem(y, 'filled');
 grid on;
 title('Auto-Correlation of the Signal');
