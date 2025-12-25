f442214f96a411adae3762d9c69d34c6e4087105
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 21:39:36 -0500

    DSIP

--- DSIP Lab/Experiment-7/Contrast_Stretching.m
+++ DSIP Lab/Experiment-7/Contrast_Stretching.m
@@ -1,35 +1,42 @@
-% == == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == ==
-    = % DIGITAL SIGNAL AND IMAGE PROCESSING LAB % % Experiment No. : 7 %
-      Experiment Title : Contrast Stretching,
-    Dynamic Range Compression % and Bit Plane Slicing % %
-        Aim : % Write a program to implement Contrast Stretching,
-    Dynamic Range % Compression and Bit Plane Slicing.% %
-                Student Details : % Name : Amey Thakur % Roll No. : 50 %
-                                           Class : COMPS -
-            BE - B -
-            50 % Batch : B3 % % Date of Experiment : 08 / 09 / 2021 %
-                         Date of Submission : 08 / 09 / 2021 % %
-                         Author : % Amey Thakur % GitHub Profile : %
-                                                                   https
-    : // github.com/Amey-Thakur
-      %
-      % Repository : %
-                     https
-    : // github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
-      % ==
-        == == == == == == == == == == == == == == == == == == == == == == == ==
-        == == == == == == == == ==
-    =
+% =====================================================================
+% DIGITAL SIGNAL AND IMAGE PROCESSING LAB
+%
+% Experiment No. : 7
+% Experiment Title : Contrast Stretching, Dynamic Range Compression
+%                    and Bit Plane Slicing
+%
+% Aim:
+% Write a program to implement Contrast Stretching, Dynamic Range
+% Compression and Bit Plane Slicing.
+%
+% Student Details:
+% Name     : Amey Thakur
+% Roll No. : 50
+% Class    : COMPS-BE-B-50
+% Batch    : B3
+%
+% Date of Experiment : 08/09/2021
+% Date of Submission : 08/09/2021
+%
+% Author:
+% Amey Thakur
+% GitHub Profile:
+% https://github.com/Amey-Thakur
+%
+% Repository:
+% https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% =====================================================================
 
-        % Clear workspace,
-    close all figures, and clear command window clc;
+% Clear workspace, close all figures, and clear command window
+clc;
 clear all;
 close all;
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Read Input Image %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -r = imread('cameraman.tif');
-% Read grayscale image[m, n] = size(r);                 % Image dimensions
+% -----------------------------
+% Read Input Image
+% -----------------------------
+r = imread('cameraman.tif');      % Read grayscale image
+[m, n] = size(r);                 % Image dimensions
 
 % -----------------------------
 % Input Parameters for Contrast Stretching
@@ -39,8 +46,10 @@ r2 = input('Enter r2 : ');
 s1 = input('Enter s1 : ');
 s2 = input('Enter s2 : ');
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Compute Transformation Slopes %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -a = s1 / r1;
+% -----------------------------
+% Compute Transformation Slopes
+% -----------------------------
+a = s1 / r1;
 b = (s2 - s1) / (r2 - r1);
 c = (255 - s2) / (255 - r2);
 
@@ -51,12 +60,18 @@ for i = 1:m
     for j = 1:n
         if r(i,j) < r1
             s(i,j) = a * r(i,j);
-elseif r(i, j) < r2 s(i, j) = b * (r(i, j) - r1) + s1;
-else s(i, j) = c * (r(i, j) - r2) + s2;
-end end end
+        elseif r(i,j) < r2
+            s(i,j) = b * (r(i,j) - r1) + s1;
+        else
+            s(i,j) = c * (r(i,j) - r2) + s2;
+        end
+    end
+end
 
-    % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Display Results %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
+% -----------------------------
+% Display Results
+% -----------------------------
+figure;
 imshow(r);
 title('Original Image');
 
