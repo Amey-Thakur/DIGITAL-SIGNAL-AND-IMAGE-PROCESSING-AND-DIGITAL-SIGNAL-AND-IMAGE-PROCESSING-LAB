9480c16bfec33954d69720031100fb27bec7433a
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 21:21:22 -0500

    DSIP

--- DSIP Lab/Experiment-5/Radix2_DIT_FFT.m
+++ DSIP Lab/Experiment-5/Radix2_DIT_FFT.m
@@ -1,56 +1,66 @@
-% == == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == ==
-    = % DIGITAL SIGNAL AND IMAGE PROCESSING LAB % % Experiment No. : 5 %
-          Experiment Title : Radix -
-      2 DIT FFT Algorithm % % Aim : % Write a program to implement the Radix -
-      2 Decimation - In - Time(DIT) % Fast Fourier Transform(FFT)
-algorithm to find the DFT of a given % sequence x(n).%
-            % Student Details : % Name : Amey Thakur % Roll No. : 50 %
-                                         Class : COMPS
-        - BE - B -
-        50 % Batch : B3 % % Date of Experiment : 25 / 08 /
-                     2021 % Date of Submission : 25 / 08 / 2021 %
-                     % Author : % Amey Thakur % GitHub Profile : %
-                                                                 https
-    : // github.com/Amey-Thakur
-      %
-      % Repository : %
-                     https
-    : // github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
-      %
-    == == == == == == == == == == == == == == == == == == == == == == == == ==
-    == == == == == == == == ==
-    =
+% =====================================================================
+% DIGITAL SIGNAL AND IMAGE PROCESSING LAB
+%
+% Experiment No. : 5
+% Experiment Title : Radix-2 DIT FFT Algorithm
+%
+% Aim:
+% Write a program to implement the Radix-2 Decimation-In-Time (DIT)
+% Fast Fourier Transform (FFT) algorithm to find the DFT of a given
+% sequence x(n).
+%
+% Student Details:
+% Name     : Amey Thakur
+% Roll No. : 50
+% Class    : COMPS-BE-B-50
+% Batch    : B3
+%
+% Date of Experiment : 25/08/2021
+% Date of Submission : 25/08/2021
+%
+% Author:
+% Amey Thakur
+% GitHub Profile:
+% https://github.com/Amey-Thakur
+%
+% Repository:
+% https://github.com/Amey-Thakur/DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-AND-DIGITAL-SIGNAL-AND-IMAGE-PROCESSING-LAB
+% =====================================================================
 
-        % Clear command window,
-    workspace, and close all figures clc;
+% Clear command window, workspace, and close all figures
+clc;
 clear all;
 close all;
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Input Sequence %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -xn =
-    input('Enter the input sequence: ');
-% Input discrete - time sequence
+% -----------------------------
+% Input Sequence
+% -----------------------------
+xn = input('Enter the input sequence: ');   % Input discrete-time sequence
 
-                       % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -%
-                       Plot Input Sequence %
-                       -- -- -- -- -- -- -- -- -- -- -- -- -- -- -figure;
-subplot(2, 1, 1);
+% -----------------------------
+% Plot Input Sequence
+% -----------------------------
+figure;
+subplot(2,1,1);
 stem(xn, 'filled');
 grid on;
 xlabel('Sample Index');
 ylabel('Amplitude');
 title('Input Sequence');
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% FFT Computation %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -xk = fft(xn);
-% FFT of the input sequence
+% -----------------------------
+% FFT Computation
+% -----------------------------
+xk = fft(xn);              % FFT of the input sequence
 
-    % Display FFT result in command window disp('The resultant FFT is:');
+% Display FFT result in command window
+disp('The resultant FFT is:');
 disp(xk);
 
-% -- -- -- -- -- -- -- -- -- -- -- -- -- -- -% Plot FFT Output %
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -subplot(2, 1, 2);
+% -----------------------------
+% Plot FFT Output
+% -----------------------------
+subplot(2,1,2);
 plot(xk, 'o');
 grid on;
 xlabel('Real Axis');
